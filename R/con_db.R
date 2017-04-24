8cd0ee68ff6684e8ac6a9900e0e13a31cc1f7e01
Daniel Chen <Maveri2k@yahoo.com>
2017-04-24 12:56:05 -0400

    :tada:

---
+++ R/con_db.R
@@ -0,0 +1,27 @@
+con_db <- function(db, user, pass, driver = 'PostgreSQL',
+                   host = 'localhost', port = 5432, close_existing_cons = TRUE) {
+    if (driver = 'PostgreSQL') {
+        con_db_postgresql(db, user, pass, host, port, close_existing_cons)
+    } else{
+        stop("Unknown driver")
+    }
+
+}
+
+con_db_postgresql <- function(db, user, pass, host, port, close_existing_cons){
+    if (require(RPostgreSQL)) {
+        drv <- RPostgreSQL::dbDriver("PostgreSQL")
+
+        if (close_existing_cons) {
+            all_cons <- RPostgreSQL::dbListConnections(drv)
+            for (con in all_cons) {RPostgreSQL::dbDisconnect(con)}
+        }
+
+        con <- RPostgreSQL::dbConnect(drv, dbname = db,
+                                      host = host, port = port,
+                                      user = user, password = pass)
+        return(con)
+    } else {
+        stop('You need to install the "RPostgreSQL" library')
+    }
+}
