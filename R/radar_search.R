eeeb4052ae3b689de3ce64b6c8e131de0c9b9387
Daniel Chen <Maveri2k@yahoo.com>
2017-05-25 00:42:43 +0000

    radar_search

---
+++ R/radar_search.R
@@ -0,0 +1,17 @@
+#' Performs a radar search from a given lat and lon within a certain radius
+#'
+#' Uses the google maps api which will require an API key
+#'
+#' @param lat the latitude
+#' @param lon the longitude
+#' @param type_search_string the string to be used to look for location types
+#' @param radius how wide the radar search will look
+#' @param ... optional. parameters passed into sdal::get_google_maps_key
+#' @export
+radar_search <- function(lat, lon, type_search_string, radius = 1609, ...) {
+    jsonlite::fromJSON(paste0("https://maps.googleapis.com/maps/api/place/radarsearch/json?",
+                    "location=", lat, ",", lon,
+                    "&radius=", radius,
+                    "&type=", type_search_string,
+                    "&key=", sdalr::get_google_maps_key(...)))
+}
