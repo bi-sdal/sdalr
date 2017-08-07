23c91ee6ede932d7341408beffd4ccc5e1bacb84
Daniel Chen <Maveri2k@yahoo.com>
2017-08-07 11:08:08 -0400

    add function to get acs api key (Kyle Morgan's)

---
+++ R/get_acs_key.R
@@ -0,0 +1,14 @@
+#' Retrieves the ACS API key on the server
+#'
+#' So it's not accidentently pushed in the code
+#'
+#' @param file location of the google maps key file by default it points to the location in the sdal server
+#' @return string
+#' @export
+#' @examples
+#' get_acs_key()
+#'
+#' get_acs_key('~/acs_api_key.txt')
+get_acs_key <- function(file='/home/sdal/projects/keys/acs_api_key.txt') {
+    paste(readLines(file), collapse = " ")
+}
