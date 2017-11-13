5ff75e3a715f82d99eedaa52d93da6ed75c19686
Daniel Chen <Maveri2k@yahoo.com>
2017-11-13 17:10:12 +0000

    clean up docs, unname params

--- R/create_vault_sdal.R
+++ R/create_vault_sdal.R
@@ -3,6 +3,7 @@ library(secret)
 #' Create the default sdal vault
 #'
 #' mainly useful for testing or after deleting the vault
+#' @param vault, string, location of the vault, defaults to /home/sdal/projects/sdal/vault
 #' @export
 create_vault_sdal <- function(vault = '/home/sdal/projects/sdal/vault/') {
     create_vault(vault)
