d1704af0e476a022aa98425c6fd7dada4b73d94b
Daniel Chen <Maveri2k@yahoo.com>
2018-06-11 15:00:44 +0000

    clean up con_db documentation and code

--- R/con_db.R
+++ R/con_db.R
@@ -7,11 +7,16 @@
 #' @param dbname the name of the database you want to connect to, defaults to current user
 #' @param user the username for the database connection, defaults to current user
 #' @param driver the database driver to use, defaults to 'PostgreSQL'
-#' @param host where the database is running, defaults to 'postgresql'
+#' @param host where the database is running, defaults to 'postgresql (the docker container name)'
 #' @param port the port where the database in running, defaults to '5432' for PostgreSQL
-#' @param close_existing_cons whether to close existing database connections, defaults to 'TRUE'
-#' @param pass optional. if .dbpass exists in users home directory, password will be read from file, 
-#' else directly pass a password to the database without rstudio password prompt
+#' @param close_existing_cons whether to close existing database connections, defaults to 'TRUE'.
+#'                            You need to set this to FALSE if
+#'                            you are planning to make simultaneous database connections.
+#' @param pass optional, defaults to NULL.
+#'             If .dbpass exists in users home directory, password will be read from file (not advised).
+#'             It will then try to run the `sdalr::get_my_password()` function,
+#'             if that fails it will prompt the user for a password.
+#'             You can also directly pass in a password as a string (not advised).
 #' @return database connection
 #' @export
 #' @examples
@@ -26,50 +31,50 @@ con_db <- function(dbname = Sys.getenv("LOGNAME"), user = Sys.getenv("LOGNAME"),
                    host = 'postgresql', port = 5432, close_existing_cons = TRUE,
                    pass = NULL) {
 
+  # try to guess password if nothing is passed in
   if (is.null(pass)) {
     if (file.exists(sprintf("/home/%s/.dbpass", Sys.getenv("LOGNAME")))) {
+      warning('You are storing a plain text file as your password,
+              please run sdalr::setup_user_pass() or sdalr::update_user_pass(),
+              and delete the ~/.dbpass file.')
       pass <- readLines(sprintf("/home/%s/.dbpass", Sys.getenv("LOGNAME")))
-    }
-  }  
-  
-  if (is.null(user)) {
-        user <- getPass::getPass("database username")
+    } else if (!is.na(get_my_password())) {
+      pass <- get_my_password()
     } else {
-        user <- user
+      pass <- getPass("LDAP Password (the one you use to login to Lightfoot and RStudio):")
     }
+  } else {
+    user <- user
+    pass <- pass
+  }
 
-    if (is.null(pass)) {
-        pass <- getPass::getPass("database password")
-    } else {
-        pass <- pass
-    }
-
-    if (driver == 'PostgreSQL') {
-        sdalr::con_db_postgresql(dbname, user, pass, host, port, close_existing_cons)
-    } else{
-        stop("Unknown driver")
-    }
+  # create the database connection
+  if (driver == 'PostgreSQL') {
+    sdalr::con_db_postgresql(dbname, user, pass, host, port, close_existing_cons)
+  } else{
+    stop("Unknown driver")
+  }
 }
 
 #' Makes a connection to a postgresql database
 #' @return postgresql database connection
 #' @export
 con_db_postgresql <- function(dbname, user, pass, host, port, close_existing_cons) {
-    drv <- RPostgreSQL::PostgreSQL()
+  drv <- RPostgreSQL::PostgreSQL()
 
-    if (close_existing_cons) {
-        all_cons <- RPostgreSQL::dbListConnections(drv)
-        for (con in all_cons) {RPostgreSQL::dbDisconnect(con)}
-    }
+  if (close_existing_cons) {
+    all_cons <- RPostgreSQL::dbListConnections(drv)
+    for (con in all_cons) {RPostgreSQL::dbDisconnect(con)}
+  }
 
-    params <- list(drv = drv,
-                   dbname = dbname,
-                   host = host,
-                   port = port,
-                   user = user,
-                   password = pass)
+  params <- list(drv = drv,
+                 dbname = dbname,
+                 host = host,
+                 port = port,
+                 user = user,
+                 password = pass)
 
-    # create connection
-    con <- do.call(DBI::dbConnect, params)
-    return(con)
+  # create connection
+  con <- do.call(DBI::dbConnect, params)
+  return(con)
 }
